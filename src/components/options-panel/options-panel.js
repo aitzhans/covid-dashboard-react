17524aa5c966708f04af7c03b13a4a12a86e09bf
aitzhans <aitzhan@mail.ru>
2021-02-28 21:39:22 +0600

    refactor: refactor options component to function instead of class

--- src/components/options-panel/options-panel.js
+++ src/components/options-panel/options-panel.js
@@ -1,83 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { selectedCriteriaUpdated } from '../../actions';
 
-class OptionsPanel extends Component {
-  onChangeValue(event) {
-    const selected = event.target;
-    this.clearOptions();
-    selected.classList.toggle('options__opt--selected');
-    // console.log(selected.getAttribute('value'));
-    this.props.selectedCriteriaUpdated(selected.getAttribute('value'));
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  clearOptions() {
+const OptionsPanel = ({ selectedCriteriaUpdated }) => {
+  const clearOptions = () => {
     const opts = document.querySelectorAll('.options__opt');
     opts.forEach((opt) => {
       opt.classList.remove('options__opt--selected');
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="content__options  options  options--countries" onClick={(e) => this.onChangeValue(e)}>
-        <div
-          className="options__opt"
-          value="totalConfirmed"
-        >
-          Cases
-        </div>
-        <div
-          className="options__opt"
-          value="totalDeaths"
-        >
-          Deaths
-        </div>
-        <div
-          className="options__opt"
-          value="totalRecovered"
-        >
-          Recovered
-        </div>
+  const onChangeValue = (event) => {
+    const selected = event.target;
+    clearOptions();
+    selected.classList.toggle('options__opt--selected');
+    selectedCriteriaUpdated(selected.getAttribute('value'));
+  };
+
+  return (
+    <div className="content__options  options  options--countries" onClick={(e) => onChangeValue(e)}>
+      <div
+        className="options__opt  options__opt--selected"
+        value="totalConfirmed"
+      >
+        Cases
       </div>
-    );
-  }
-
-  // render() {
-  //   return (
-  //     <div className="content__options  options  options--countries" onChange={(e) => this.onChangeValue(e)}>
-  //       <input
-  //         className="options__input"
-  //         value="totalConfirmed"
-  //         type="radio"
-  //         name="countries-options"
-  //         id="countries-cases"
-  //         defaultChecked
-  //       />
-  //       <label htmlFor="countries-cases" className="options__label">Cases</label>
-  //       <input
-  //         className="options__input"
-  //         value="totalDeaths"
-  //         type="radio"
-  //         name="countries-options"
-  //         id="countries-deaths"
-  //       />
-  //       <label htmlFor="countries-deaths" className="options__label">Deaths</label>
-  //       <input
-  //         className="options__input"
-  //         value="totalRecovered"
-  //         type="radio"
-  //         name="countries-options"
-  //         id="countries-recovered"
-  //       />
-  //       <label htmlFor="countries-recovered" className="options__label">Recovered</label>
-  //     </div>
-  //   );
-  // }
-}
-
-// export default OptionsPanel;
+      <div
+        className="options__opt"
+        value="totalDeaths"
+      >
+        Deaths
+      </div>
+      <div
+        className="options__opt"
+        value="totalRecovered"
+      >
+        Recovered
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ selectedCriteria }) => ({
   selectedCriteria
